9e0b8bd518ebaa6fe37a7648eaef038a6e17b95c
DKorir <dominic.korir@student.moringaschool.com>
2022-01-05 12:09:41 +0300

    initial commit add components upvote, downvote, delete for buttons

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { UpvoteComponent } from './upvote/upvote.component';
+import { DownvoteComponent } from './downvote/downvote.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
+    UpvoteComponent,
+    DownvoteComponent,
+    DeleteComponent,
   ],
   imports: [
     BrowserModule,
